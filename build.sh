2ba55caf78df5c22638fa889fd5e58804dbdff95
Eugene Manuilov <eugene.manuilov@10up.com>
2015-05-20 18:13:58 +0300

    Fixed lines ending issues

--- build.sh
+++ build.sh
@@ -6,22 +6,24 @@ NC='\033[0m'
 
 IMAGE=$(docker images | grep "10up/nginx")
 if [ -z $IMAGE ]; then
-    printf "${GREEN}Build 10up/nginx image...${NC}"
+    printf "${GREEN}Build 10up/nginx image...${NC}\n"
     docker build -q -t "10up/nginx" .
 fi
 
-printf "${GREEN}Start the container and run build process...${NC}"
+printf "${GREEN}Start the container and run build process...${NC}\n"
 CONTAINERID=$(docker run -d 10up/nginx build-nginx)
 
 EXITCODE=$(docker wait $CONTAINERID)
 if [ $EXITCODE -ne 0 ]; then
-    printf "${RED}Build process returned not empty code...${NC}"
+    printf "${RED}Build process returned not empty code...${NC}\n"
 else
-    printf "${GREEN}Copy RMPs out of the container...${NC}"
+    printf "${GREEN}Copy RMPs out of the container...${NC}\n"
     docker cp $CONTAINERID:/root/rpmbuild/RPMS ./
+    echo "RPMS copied..."
     docker cp $CONTAINERID:/root/rpmbuild/SRPMS ./
+    echo "SRPMS copied..."
 fi
 
-printf "${GREEN}Remove the container...${NC}"
+printf "${GREEN}Remove the container...${NC}\n"
 docker stop $CONTAINERID
 docker rm $CONTAINERID
