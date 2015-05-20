ebf8db4edd81fb75c02f6c4c694e262006a69a05
Eugene Manuilov <eugene.manuilov@10up.com>
2015-05-20 18:08:00 +0300

    Beautified build script

--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+IMAGE=$(docker images | grep "10up/nginx")
+if [ -z $IMAGE ]; then
+    printf "${GREEN}Build 10up/nginx image...${NC}"
+    docker build -q -t "10up/nginx" .
+fi
+
+printf "${GREEN}Start the container and run build process...${NC}"
 CONTAINERID=$(docker run -d 10up/nginx build-nginx)
 
-docker wait $CONTAINERID
+EXITCODE=$(docker wait $CONTAINERID)
+if [ $EXITCODE -ne 0 ]; then
+    printf "${RED}Build process returned not empty code...${NC}"
+else
+    printf "${GREEN}Copy RMPs out of the container...${NC}"
+    docker cp $CONTAINERID:/root/rpmbuild/RPMS ./
+    docker cp $CONTAINERID:/root/rpmbuild/SRPMS ./
+fi
 
-docker cp $CONTAINERID:/root/rpmbuild/RPMS ./
-docker cp $CONTAINERID:/root/rpmbuild/SRPMS ./
-
+printf "${GREEN}Remove the container...${NC}"
 docker stop $CONTAINERID
 docker rm $CONTAINERID
