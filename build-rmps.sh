40497428fbed428a28a05f2b83b3496b8136f83c
Eugene Manuilov <eugene.manuilov@10up.com>
2015-05-26 14:59:39 +0300

    Extracted nginx build script into separate script

--- build-rmps.sh
+++ build-rmps.sh
@@ -1,37 +1,3 @@
 #!/bin/bash
 
-NAME=10up/nginx
-VERSION=1.0.0
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-printf "${GREEN}===> Build Nginx...${NC}\n"
-
-IMAGE=$(docker images ${NAME} | awk '{ print $2 }' | grep -F "${VERSION}")
-if [ -z $IMAGE ]; then
-	pushd docker-images/nginx
-	printf "${GREEN}===> Build 10up/nginx image...${NC}\n"
-	docker build -q -t "${NAME}:${VERSION}" --rm .
-	popd
-fi
-
-printf "${GREEN}===> Start the container and run build process...${NC}\n"
-CONTAINERID=$(docker run -d ${NAME}:${VERSION})
-echo "Waiting until container finishes running..."
-
-EXITCODE=$(docker wait $CONTAINERID)
-if [ $EXITCODE -ne 0 ]; then
-	printf "${RED}===> Build process returned not empty code...${NC}\n"
-else
-	printf "${GREEN}===> Copy RMPs out of the container...${NC}\n"
-	docker cp $CONTAINERID:/root/rpmbuild/RPMS ./repo/
-	echo "RPMS copied..."
-	docker cp $CONTAINERID:/root/rpmbuild/SRPMS ./repo/
-	echo "SRPMS copied..."
-fi
-
-printf "${GREEN}===> Remove the container...${NC}\n"
-docker stop $CONTAINERID
-docker rm $CONTAINERID
+./docker-images/nginx.sh
