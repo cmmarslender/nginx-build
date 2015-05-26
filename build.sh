f53dbfb4c83cad16733bc1c81e77a76c803a6b9a
Eugene Manuilov <eugene.manuilov@10up.com>
2015-05-26 14:05:21 +0300

    Moved Nginx build image into builds folder. Updated build.sh script to use new folder structure and save RPMs in the repo folder.

--- build.sh
+++ build.sh
@@ -6,24 +6,30 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-IMAGE=$(docker images | grep "10up/nginx")
+pushd builds/nginx
+
+printf "${GREEN}===> Build Nginx...${NC}\n"
+
+IMAGE=$(docker images | grep "10up/nginx.*${VERSION}")
 if [ -z $IMAGE ]; then
     printf "${GREEN}===> Build 10up/nginx image...${NC}\n"
     docker build -q -t "10up/nginx:${VERSION}" .
 fi
 
+popd
+
 printf "${GREEN}===> Start the container and run build process...${NC}\n"
 CONTAINERID=$(docker run -d 10up/nginx:${VERSION})
-echo "Done..."
+echo "Waiting until container finishes running..."
 
 EXITCODE=$(docker wait $CONTAINERID)
 if [ $EXITCODE -ne 0 ]; then
     printf "${RED}===> Build process returned not empty code...${NC}\n"
 else
     printf "${GREEN}===> Copy RMPs out of the container...${NC}\n"
-    docker cp $CONTAINERID:/root/rpmbuild/RPMS ./
+    docker cp $CONTAINERID:/root/rpmbuild/RPMS ./repo/
     echo "RPMS copied..."
-    docker cp $CONTAINERID:/root/rpmbuild/SRPMS ./
+    docker cp $CONTAINERID:/root/rpmbuild/SRPMS ./repo/
     echo "SRPMS copied..."
 fi
 
