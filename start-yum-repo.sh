c293096e32b37debc14142ca79b83595943bc41e
Eugene Manuilov <eugene.manuilov@10up.com>
2015-05-29 10:19:00 +0300

    Fixed formatting and added full path to the directory with yum-repo docker file.

--- start-yum-repo.sh
+++ start-yum-repo.sh
@@ -2,7 +2,7 @@
 
 NAME=10up/yum
 VERSION=1.0.0
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 GREEN='\033[0;32m'
 NC='\033[0m'
@@ -11,7 +11,7 @@ printf "${GREEN}===> Start yum repository...${NC}\n"
 
 IMAGE=$(docker images ${NAME} | awk '{ print $2 }' | grep -F "${VERSION}")
 if [ -z $IMAGE ]; then
-	pushd yum-repo > /dev/null
+	pushd ${DIR}/yum-repo > /dev/null
 	printf "${GREEN}===> Build 10up/yum image...${NC}\n"
 	docker build -q -t "${NAME}:${VERSION}" --rm .
 	popd
