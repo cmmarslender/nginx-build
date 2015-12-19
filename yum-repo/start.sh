c5359df01df4c42eac14531e71956bb76f40f0d8
Chris Marslender <chrismarslender@gmail.com>
2015-12-18 20:21:49 -0700

    Get rid of welcome page

--- yum-repo/start.sh
+++ yum-repo/start.sh
@@ -9,6 +9,9 @@ createrepo /var/packages
 # Delete the default webroot
 rm -rf /var/www/html
 
+# Get rid of the "welcome" page
+rm /etc/httpd/conf.d/welcome.conf
+
 # Create symlink to the packages folder
 ln -s /var/packages /var/www/html
 
