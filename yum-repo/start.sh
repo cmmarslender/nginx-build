66827e8f43afaf8c45773da03a63cee8588e2df9
Chris Marslender <chrismarslender@gmail.com>
2015-12-18 20:09:19 -0700

    Remove default web root

--- yum-repo/start.sh
+++ yum-repo/start.sh
@@ -6,6 +6,9 @@ su -
 # Create repository
 createrepo /var/packages
 
+# Delete the default webroot
+rm -rf /var/www/html
+
 # Create symlink to the packages folder
 ln -s /var/packages /var/www/html
 
